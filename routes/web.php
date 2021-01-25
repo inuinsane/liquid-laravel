71a06b26ce9d7761c74e91ecf838757df185ced9
inuinsane <inuinsane@gmail.com>
2021-01-25 08:25:36 +0800

    prevent back after logout

--- routes/web.php
+++ routes/web.php
@@ -26,32 +26,35 @@ Route::get('/', function () {
 
 Route::get('/check-kode', [RoomController::class, 'check_code'])->name('check.kode');
 
-Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/liquid', [HomeController::class, 'liquid'])->name('liquid');
-Route::get('/kategori', [HomeController::class, 'kategori'])->name('view.kategori');
-
-
-Route::prefix('room')->group(function () {
-    Route::get('/', [RoomController::class, 'view'])->name('view.room');
-    Route::post('/', [RoomController::class, 'create'])->name('create.room');
-    Route::get('/{code}', [RoomController::class, 'get'])->name('get.room');
-});
-
-Route::prefix('penilaian')->group(function () {
-    Route::post('/', [PenilaianController::class, 'create'])->name('create.penilaian');
-    Route::get('/', [PenilaianController::class, 'view'])->name('view.penilaian');
-    Route::get('/{code}', [PenilaianController::class, 'get'])->name('get.penilaian');
-});
+Route::group(['middleware' => 'prevent-back'], function () {
+    Auth::routes();
+    
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/liquid', [HomeController::class, 'liquid'])->name('liquid');
+    Route::get('/kategori', [HomeController::class, 'kategori'])->name('view.kategori');
 
 
-Route::get('/close-room/{code}', [RoomController::class, 'toggleRoom'])->name('close.room');
-Route::get('/open-room/{code}', [RoomController::class, 'toggleRoom'])->name('open.room');
+    Route::prefix('room')->group(function () {
+        Route::get('/', [RoomController::class, 'view'])->name('view.room');
+        Route::post('/', [RoomController::class, 'create'])->name('create.room');
+        Route::get('/{code}', [RoomController::class, 'get'])->name('get.room');
+    });
 
-Route::prefix('kategori')->group(function () {
-    Route::get('/json', [CategoryController::class, 'json'])->name('get.category.datatable');
-    Route::post('/create', [CategoryController::class, 'create'])->name('create.category');
-    Route::get('/delete/{category}', [CategoryController::class, 'delete'])->name('delete.category');
-    Route::post('/update', [CategoryController::class, 'update'])->name('update.category');
+    Route::prefix('penilaian')->group(function () {
+        Route::post('/', [PenilaianController::class, 'create'])->name('create.penilaian');
+        Route::get('/', [PenilaianController::class, 'view'])->name('view.penilaian');
+        Route::get('/{code}', [PenilaianController::class, 'get'])->name('get.penilaian');
+    });
+
+
+    Route::get('/close-room/{code}', [RoomController::class, 'toggleRoom'])->name('close.room');
+    Route::get('/open-room/{code}', [RoomController::class, 'toggleRoom'])->name('open.room');
+
+    Route::prefix('kategori')->group(function () {
+        Route::get('/json', [CategoryController::class, 'json'])->name('get.category.datatable');
+        Route::post('/create', [CategoryController::class, 'create'])->name('create.category');
+        Route::get('/delete/{category}', [CategoryController::class, 'delete'])->name('delete.category');
+        Route::post('/update', [CategoryController::class, 'update'])->name('update.category');
+    });
 });
